961d712b73cbb1cc2e0b6c89394282891c25d86e
Manuel Gomez Mateo <creawww@gmail.com>
2018-10-27 00:12:29 +0200

    permited cors

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra/reloader' if development?
 require 'net/http'
 require 'json'
 
+before do
+   content_type :json
+   headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+end
+
 get '/' do
   if params["url"]
     url_in= URI.encode_www_form(params)
@@ -14,6 +20,8 @@ get '/' do
 end
 
 #http://localhost:3000/?url=http://www.ffcv.es/ncompeticiones/server.php?action=getEncuentrosEquipo&tmp=2018/2019&id=0201630511
+
+#https://sin-cors.herokuapp.com/?url=http://www.ffcv.es/ncompeticiones/server.php?action=getEncuentrosEquipo&tmp=2018/2019&id=0201630511
 
 def response_url(url_app)
   n_url=url_app[4..-1]
